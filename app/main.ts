b783abce97eb4f1cccdfde2d64ac7a511675fa26
HiT1000 <hiteshsingla209@gmail.com>
2025-11-11 18:20:54 +0530

    6th type

--- app/main.ts
+++ app/main.ts
@@ -5,6 +5,10 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+function equalsIgnoreCase(str1: string, str2: string): boolean {
+  return str1.toLowerCase() === str2.toLowerCase();
+}
+
 function exit(command: string): void {
   process.exit(parseInt(command, 10));
 }
@@ -13,20 +17,32 @@ function echo(command: string): void {
   console.log(command.substring(5));
 }
 
+function type(filename: string): void {
+  if (equalsIgnoreCase(filename, "echo")) {
+    console.log("echo is a shell builtin");
+  } else if (equalsIgnoreCase(filename, "exit")) {
+    console.log("exit is a shell builtin");
+  } else if (equalsIgnoreCase(filename, "type")) {
+    console.log("type is a shell builtin");
+  } else {
+    console.log(`${filename}: not found`);
+  }
+}
+
 function stepRun() {
   rl.question("$ ", (command) => {
     const trimmed = command.trim();
-    if(trimmed){
+    if (trimmed) {
       const parts = trimmed.split(/\s+/);
-      if(parts[0] === "exit"){
+      if (equalsIgnoreCase(parts[0], "exit")) {
         exit(parts[1]);
-      }
-      else if(parts[0] === "echo"){  
+      } else if (equalsIgnoreCase(parts[0], "echo")) {
         echo(trimmed);
-      }
-      else{
+      } else if (equalsIgnoreCase(parts[0], "type")) {
+        type(parts[1]);
+      } else {
         console.log(`${parts[0]}: command not found`);
-      } 
+      }
     }
     stepRun();
   });
